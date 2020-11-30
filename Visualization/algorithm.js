a0769c5b45973ce0f4cc655d26d7d865b75a0c5e
Medha Patil <medhap3@illinois.edu>
2020-11-29 19:00:46 -0600

    bandwidth with lower and upper diaginal implemented

--- Visualization/algorithm.js
+++ Visualization/algorithm.js
@@ -10,7 +10,7 @@ var getGrids = function(numRows, numCols) {
         var scoreColumns = [];
         var pointerColumns = [];
 		for (var j = 0; j < numCols; j++) {
-            scoreColumns.push(0)
+            scoreColumns.push(null)
             pointerColumns.push(ORIGIN)
         }
         scoreRows.push(scoreColumns)
@@ -19,26 +19,67 @@ var getGrids = function(numRows, numCols) {
     return [scoreRows, pointerRows]
 }
 
+var getLimits = function(sequence1, sequence2, bandwidth) {
+    var yLim = sequence1.length + 1
+    var xLim = sequence2.length + 1
+    if (bandwidth < Math.min(sequence1.length, sequence2.length)) {
+        if (sequence1.length < sequence2.length) {
+            yLim = bandwidth + 1
+            xLim = sequence2.length + 1 - (sequence1.length + 1 - bandwidth)
+        } else {
+            xLim = bandwidth + 1
+            yLim = sequence1.length + 1 - (sequence2.length + 1 - bandwidth)
+        }
+    }
+    return [xLim, yLim]
+}
+
 var calculateAlignment = function(sequence1, sequence2, bandwidth) {
-    grids = getGrids(sequence1.length + 1, sequence2.length + 1)
-    scoreGrid = grids[0]
-    pointerGrid = grids[1]
-    for (var i = 1; i < sequence1.length + 1; i++) {
+		
+    var grids = getGrids(sequence1.length + 1, sequence2.length + 1)
+    var scoreGrid = grids[0]
+    var pointerGrid = grids[1]
+    var limits = getLimits(sequence1, sequence2, bandwidth)
+    var xLim = limits[1]
+    var upperYLim = limits[0]
+    var lowerYLim = 1
+
+    scoreGrid[0][0] = 0
+    pointerGrid[0][0] = ORIGIN
+
+    for (var i = 1; i < xLim; i++) {
         scoreGrid[i][0] = scoreGrid[i - 1][0] - 1
         pointerGrid[i][0] = UP
     }
-    for (var j = 1; j < sequence2.length + 1; j++) {
+    
+    for (var j = 1; j < upperYLim; j++) {
         scoreGrid[0][j] = scoreGrid[0][j - 1] - 1
         pointerGrid[0][j] = LEFT
     }
+    
     for (var i = 1; i < sequence1.length + 1; i++) {
-        for (var j = 1; j < sequence2.length + 1; j++) {
-            insertValue = Number.NEGATIVE_INFINITY
-            deleteValue = Number.NEGATIVE_INFINITY
-            if (Math.abs(i - j) < bandwidth) {
-                insertValue = scoreGrid[i][j - 1] - 1
-                deleteValue = scoreGrid[i - 1][j] - 1
+        if (i > xLim) {
+            if (lowerYLim < sequence2.length + 1) {
+            	lowerYLim++
             }
+        }
+        if (upperYLim < sequence2.length + 1) {
+           	upperYLim++
+        }
+
+        for (var j = lowerYLim; j < upperYLim; j++) {
+        		if (!scoreGrid[i][j - 1]) {
+            		insertValue = Number.NEGATIVE_INFINITY
+            } else {
+            		insertValue = scoreGrid[i][j - 1] - 1
+            }
+            
+            if (!scoreGrid[i - 1][j]) {
+            		deleteValue = Number.NEGATIVE_INFINITY
+            } else {
+            		deleteValue = scoreGrid[i - 1][j] - 1
+            }
+            
             matchValue = 0
             if (sequence1.charAt(i - 1) == sequence2.charAt(j - 1) ) {
                 matchValue = scoreGrid[i - 1][j - 1] + 1
@@ -57,6 +98,7 @@ var calculateAlignment = function(sequence1, sequence2, bandwidth) {
                 scoreGrid[i][j] = matchValue
                 pointerGrid[i][j] = TOPLEFT
             }
+            
         }
     }
     return [scoreGrid, pointerGrid]
--- Visualization/visualization.js
+++ Visualization/visualization.js
@@ -18,10 +18,10 @@ var getPathCells = function(pointers) {
     var curY = pointers[0].length - 1
     while (curX > 0 || curY > 0) {
         path.push([curX, curY])
-        if (pointers[curX][curY][0] == LEFT[0] && pointers[curX][curY][1] == LEFT[1]) {
-            curY = curY - 1
-        } else if (pointers[curX][curY][0] == UP[0] && pointers[curX][curY][1] == UP[1]) {
+        if (pointers[curX][curY][0] == UP[0] && pointers[curX][curY][1] == UP[1]) {
             curX = curX - 1
+        } else if (pointers[curX][curY][0] == LEFT[0] && pointers[curX][curY][1] == LEFT[1]) {
+            curY = curY - 1
         } else {
             curX = curX - 1
             curY = curY - 1
@@ -111,6 +111,98 @@ var getGridData = function(numX, numY, widthPixels, heightPixels, sequenceWidth,
     }
     return data
 }
+
+var ORIGIN = [0, 0]
+var UP = [-1, 0]
+var LEFT = [0, -1]
+var TOPLEFT = [-1, -1]
+
+var getGrids = function(numRows, numCols) {
+    var scoreRows = [];
+    var pointerRows = [];
+    for (var i = 0; i < numRows; i++) {
+        var scoreColumns = [];
+        var pointerColumns = [];
+		for (var j = 0; j < numCols; j++) {
+            scoreColumns.push(null)
+            pointerColumns.push(null)
+        }
+        scoreRows.push(scoreColumns)
+        pointerRows.push(pointerColumns)
+    }
+    return [scoreRows, pointerRows]
+}
+
+var getLimits = function(sequence1, sequence2, bandwidth) {
+    var xLim = sequence1.length + 1
+    var yLim = sequence2.length + 1
+    if (bandwidth < Math.min(sequence1.length, sequence2.length)) {
+        if (sequence1.length < sequence2.length) {
+            xLim = bandwidth
+            yLim = sequence2.length + 1 - (sequence1.length + 1 - bandwidth)
+        } else {
+            yLim = bandwidth
+            xLim = sequence1.length + 1 - (sequence2.length + 1 - bandwidth)
+        }
+    }
+    return [xLim, yLim]
+}
+
+var calculateAlignment = function(sequence1, sequence2, bandwidth) {
+    var grids = getGrids(sequence1.length + 1, sequence2.length + 1)
+    var scoreGrid = grids[0]
+    var pointerGrid = grids[1]
+    var limits = getLimits(sequence1, sequence2, bandwidth)
+    var xLim = limits[0]
+    var upperYLim = limits[1]
+    var lowerYLim = 1
+
+    scoreGrid[0][0] = 0
+    pointerGrid[0][0] = ORIGIN
+
+    for (var i = 1; i < xLim; i++) {
+        scoreGrid[i][0] = scoreGrid[i - 1][0] - 1
+        pointerGrid[i][0] = UP
+    }
+    for (var j = 1; j < upperYLim; j++) {
+        scoreGrid[0][j] = scoreGrid[0][j - 1] - 1
+        pointerGrid[0][j] = LEFT
+    }
+    for (var i = 1; i < sequence1.length + 1; i++) {
+        if (i <= xLim) {
+            if (upperYLim < sequence2.length + 1) {
+                upperYLim++
+            }
+        } else if (lowerYLim < sequence2.length + 1) {
+            lowerYLim++
+        }
+        for (var j = lowerYLim; j < upperYLim; j++) {
+            insertValue = scoreGrid[i][j - 1] - 1
+            deleteValue = scoreGrid[i - 1][j] - 1
+            matchValue = 0
+            if (sequence1.charAt(i - 1) == sequence2.charAt(j - 1) ) {
+                matchValue = scoreGrid[i - 1][j - 1] + 1
+            } else {
+                matchValue = scoreGrid[i - 1][j - 1] - 1
+            }
+            minScore = Math.max(insertValue, deleteValue, matchValue)
+
+            if (minScore == deleteValue) {
+                scoreGrid[i][j] = deleteValue
+                pointerGrid[i][j] = UP
+            } else if (minScore == insertValue) {
+                scoreGrid[i][j] = insertValue
+                pointerGrid[i][j] = LEFT
+            } else {
+                scoreGrid[i][j] = matchValue
+                pointerGrid[i][j] = TOPLEFT
+            }
+            
+        }
+    }
+    return [scoreGrid, pointerGrid]
+}
+
 
 var createGrid = function(width, height, sequenceWidth, sequenceHeight, scores, pointers) {
     var heightPixels = window.innerHeight / 1.2
@@ -206,7 +298,10 @@ var createGrid = function(width, height, sequenceWidth, sequenceHeight, scores, 
 alignButton.on('click', function() {
     sequence1 = sequence1TextBox.val()
     sequence2 = sequence2TextBox.val()
-    bandwidth = bandwidthTextBox.val()
+    bandwidth = parseInt(bandwidthTextBox.val())
+    if (!bandwidth) {
+        bandwidth = Number.POSITIVE_INFINITY
+    }
     result = calculateAlignment(sequence1, sequence2, bandwidth)
     scores = result[0]
     pointers = result[1]
